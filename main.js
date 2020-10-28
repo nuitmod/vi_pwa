90de8499d3ddd138eea838c28a7e6c2afd0160b0
nuitmod <wtvfqm@gmail.com>
2020-10-28 20:21:39 +0500

    add webkit transitions and keyframes

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 $(function(){
-
+//main_menu
   $('#menu').hide();
-
   $('#hdr').on('click', function(){
     $('#menu').fadeToggle(399);
   })
-
+//hide_pdf
+//  $('#pdf_data').hide();
+  // $('#pdf').on('click', function(){
+  //   $('#pdf_data').fadeToggle(399);
+  // })
+//i_menu
+  $('#i_menu_btn').on('click', function(){
+    $('.i_menu').addClass('open')
+  })
+  $('.i_menu').on('click', function(){
+    $('.i_menu').removeClass('open')
+  })
 
 })
